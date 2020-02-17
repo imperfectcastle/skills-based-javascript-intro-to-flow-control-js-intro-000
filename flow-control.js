721023908c2f282038b2fb3a87640a73701f7b21
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-17 23:48:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,32 +23,14 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   return (age >= 13 && age <= 20) ? "You are a teenager" : "You are not a teenager"
+
 }
 
 function switchAge(age) {
   switch(age) {
-    case 13:
-    console.log("You are a teenager");
-    break;
-    case 14:
-    console.log("You are a teenager");
-    break;
-    case 15:
-    console.log("You are a teenager");
-    break;
-    case 16:
-    console.log("You are a teenager");
-    break;
-    case 17:
-    console.log("You are a teenager");
-    break;
-    case 18:
-    console.log("You are a teenager");
-    break;
-    case 19:
-    console.log("You are a teenager");
-    break;
-    default;
-    console.log("You have an age");
+    case (age >= 13 && age <= 20):
+      return "You are a teenager"
+    default:
+      return "You have an age";
    }
 }
