8dd1767bdb3074b1680a9f095b16d5a8110694e5
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-17 23:33:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,6 +30,7 @@ function switchAge(age) {
   switch(age) {
     case (13, 14, 15, 16, 17, 18, 19):
       console.log ("You are a teenager");
+      break;
     default:
      return ("You have an age");
    }
